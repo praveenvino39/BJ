7b437ef307e32c40f45a221ef2d5a1c7d331b17a
praveen vino <praveenvino@praveens-MacBook-Pro.local>
2024-08-08 22:21:27 +0530

    update for ios added back

--- lib/utils/update_utils.dart
+++ lib/utils/update_utils.dart
@@ -2,6 +2,9 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:in_app_update/in_app_update.dart';
+import 'package:new_version/new_version.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:wallet_cryptomask/config.dart';
 import 'package:wallet_cryptomask/ui/shared/wallet_button.dart';
 import 'package:wallet_cryptomask/ui/utils/spaces.dart';
 
@@ -39,71 +42,71 @@ checkForUpdate(BuildContext context) {
         debugPrint(e.toString());
       });
     }
-    // if (Platform.isIOS) {
-    //   final newVersion = NewVersion();
-    //   newVersion.getVersionStatus().then((status) {
-    //     if (status != null && status.canUpdate) {
-    //       showDialog(
-    //         barrierDismissible: false,
-    //         context: context,
-    //         builder: (context) => PopScope(
-    //           canPop: false,
-    //           child: AlertDialog(
-    //             title: const Text("Update available"),
-    //             content: Column(
-    //               crossAxisAlignment: CrossAxisAlignment.start,
-    //               mainAxisSize: MainAxisSize.min,
-    //               children: [
-    //                 const Text(
-    //                     "New version of $appName is available on App Store."),
-    //                 addHeight(SpacingSize.s),
-    //                 Row(
-    //                   children: [
-    //                     const Text(
-    //                       'Current version: ',
-    //                       style: TextStyle(fontWeight: FontWeight.bold),
-    //                     ),
-    //                     Expanded(child: Text(status.localVersion)),
-    //                   ],
-    //                 ),
-    //                 Row(
-    //                   children: [
-    //                     const Text(
-    //                       'Available version: ',
-    //                       style: TextStyle(fontWeight: FontWeight.bold),
-    //                     ),
-    //                     Expanded(child: Text(status.storeVersion)),
-    //                   ],
-    //                 ),
-    //                 addHeight(SpacingSize.s),
-    //                 const Text(
-    //                   "What's new :",
-    //                   style: TextStyle(fontWeight: FontWeight.bold),
-    //                 ),
-    //                 Text(status.releaseNotes ??
-    //                     "Improved performance and stability."),
-    //                 addHeight(SpacingSize.m),
-    //                 WalletButton(
-    //                     textContent: "Update",
-    //                     onPressed: () async {
-    //                       debugPrint(status.appStoreLink);
-    //                       if (!await launchUrl(
-    //                         Uri.parse(status.appStoreLink),
-    //                         mode: LaunchMode.externalApplication,
-    //                       )) {
-    //                         throw 'Could not launch ${status.appStoreLink}';
-    //                       }
-    //                     })
-    //               ],
-    //             ),
-    //           ),
-    //         ),
-    //       );
-    //     }
-    //   }).catchError((e) {
-    //     debugPrint(e.toString());
-    //   });
-    // }
+    if (Platform.isIOS) {
+      final newVersion = NewVersion();
+      newVersion.getVersionStatus().then((status) {
+        if (status != null && status.canUpdate) {
+          showDialog(
+            barrierDismissible: false,
+            context: context,
+            builder: (context) => PopScope(
+              canPop: false,
+              child: AlertDialog(
+                title: const Text("Update available"),
+                content: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Text(
+                        "New version of $appName is available on App Store."),
+                    addHeight(SpacingSize.s),
+                    Row(
+                      children: [
+                        const Text(
+                          'Current version: ',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        Expanded(child: Text(status.localVersion)),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        const Text(
+                          'Available version: ',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        Expanded(child: Text(status.storeVersion)),
+                      ],
+                    ),
+                    addHeight(SpacingSize.s),
+                    const Text(
+                      "What's new :",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Text(status.releaseNotes ??
+                        "Improved performance and stability."),
+                    addHeight(SpacingSize.m),
+                    WalletButton(
+                        textContent: "Update",
+                        onPressed: () async {
+                          debugPrint(status.appStoreLink);
+                          if (!await launchUrl(
+                            Uri.parse(status.appStoreLink),
+                            mode: LaunchMode.externalApplication,
+                          )) {
+                            throw 'Could not launch ${status.appStoreLink}';
+                          }
+                        })
+                  ],
+                ),
+              ),
+            ),
+          );
+        }
+      }).catchError((e) {
+        debugPrint(e.toString());
+      });
+    }
   } catch (e) {
     debugPrint(e.toString());
   }
